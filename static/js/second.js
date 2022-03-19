c04befed30b753600b52e297a1d73aa73c014425
monyonko <monyonko.miriam@gmail.com>
2022-03-19 16:57:12 +0300

    first commit

---
+++ static/js/second.js
@@ -0,0 +1,32 @@
+$(document).ready(function(){
+  
+    $('.nextBtn').on('click', function(){
+        const currentImage =$('.active');
+        const firstImage =  $('.carousel2 > .slide:first')
+        const nextImage = currentImage.next()
+
+        if(nextImage.length){
+            currentImage.removeClass('active').css('z-index', -10);
+            nextImage.addClass('active').css('z-index', 10);
+        }
+        else{
+            currentImage.removeClass('active').css('z-index', -10);
+            firstImage.addClass('active').css('z-index', 10);
+        }
+        
+
+    });
+    $('.prevBtn').on('click', function(){
+        const currentImage =$('.active');
+        const prevImage = currentImage.prev()
+
+        if(prevImage.length){
+            currentImage.removeClass('active').css('z-index', -10);
+            prevImage.addClass('active').css('z-index', 10);
+        }
+    });
+   
+    
+   
+    
+})
